8f19217c46be326ebccbf94057f8521245f87eb8
Ivan Lazunin <code.wibbn@gmail.com>
2022-04-08 23:04:51 +0300

    third lab

---
+++ part2/lab3/googlenet.py
@@ -0,0 +1,98 @@
+# Импортируем основные модули pytorch
+import torch
+import torch.nn as nn
+from torch.utils.data import DataLoader
+
+# Импортируем модули, связанные с компьютерным зрением
+from torchvision import datasets
+import torchvision.transforms as T  # пакет transforms будет доступен под именем T
+from torchvision.models import googlenet  # импортируем модель GoogLeNet
+
+# Импортируем вспомогательные модули
+from sklearn.metrics import accuracy_score
+import matplotlib.pyplot as plt
+
+
+# В данной лабораторной работе предлагается дообучить предобученные модели на уже известном нам наборе CIFAR10.
+# В этой части предлагается поработать с архитектурой GoogLeNet.
+# Необходимо:
+# 1. Подготовить изображения (см. ниже);
+# 2. "Заморозить" веса предобученной модели;
+# 3. Заменить последний слой для соответствия размерности выхода 10-яти классам;
+# 4. Дообучить модель.
+
+model = googlenet(pretrained=True)  # загружаем модель с предобученными весами
+# Здесь необходимо выполнить 2-ой и 3-ий пункты.
+# ВАШ КОД ЗДЕСЬ
+
+# ===============
+
+# Последовательность преобразований исходных изображений. Нужно заполнить.
+# Смысл этих преобразований в том, чтобы привести входные изображения из набора CIFAR10 к тому виду, который был у
+# изображений из набора, на котором была предобучена сеть. А именно:
+# 1. Картинки 3-ех канальные с размером 224 х 224 пикселей.
+# 2. Картики должны быть преобразованы в тензор и иметь цветовые компоненты на отрезке [0; 1].
+# 3. Среднее по цветовым каналам должно быть [0.485, 0.456, 0.406], а среднеквадратическое отклонение [0.229, 0.224, 0.225].
+# Посмотрите документацию по классам Compose, Resize, CenterCrop, ToTensor, Normalize из пакета torchvision.transforms.
+transforms = T.Compose(
+    # ВАШ КОД ЗДЕСЬ
+    ...
+    # ===============
+)
+
+# Задаем количество эпох (проходов по всей обучающей выборке) и размер пакета, можно варьировать
+EPOCHS = 10
+BATCH_SIZE = 256
+
+# Загружаем данные из набора CIFAR10
+train_data = datasets.CIFAR10(root='./data/train', train=True, download=True, transform=transforms)
+test_data = datasets.CIFAR10(root='./data/test', train=False, download=True, transform=transforms)
+
+# DataLoader позволяет разбить выборку на пакеты заданного размера.
+# Параметр shuffle отвечает за перемешивание данных в пакете
+train_loader = DataLoader(train_data, batch_size=BATCH_SIZE, shuffle=True)
+
+# Если графический ускоритель поддерживает обучение на нем, будем использовать его,
+# иначе обучать на процессоре.
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+# Готовимся к обучению
+model = model.to(device)  # переносим модель на доступное устройство
+optimizer = ...  # оптимизатор, нужно выбрать и настроить
+loss_function = ...  # функция потерь, нужно выбрать
+loss_history = list()  # список для хранения истории изменения функции стоимости
+
+# Начинаем обучение
+for epoch in range(EPOCHS):
+    for i, (batch, labels) in enumerate(train_loader):  # разбиваем выборку на пакеты
+        # Нужно реализовать один шаг градиентного спуска
+        loss = torch.tensor(0, dtype=torch.float32)  # значение функции стоимости на пакете, нужно рассчитать
+        # ВАШ КОД ЗДЕСЬ
+        # ===============
+        loss_history.append(loss.log().item())  # добавляется логарифм стоимости для большей наглядности
+    print(f'Эпоха {epoch + 1} завершилась с функцией стоимости на последнем пакете = {loss.item()}')
+
+
+# Выводим график функции стоимости
+plt.title('Зависимость функции стоимости от номера шага градиентного спуска')
+plt.xlabel('Номер шага')
+plt.ylabel('Функция стоимости')
+plt.plot(loss_history)
+plt.show()
+
+# Отключаем расчет вычислительного графа для экономии времени и памяти: нам не нужно считать градиенты при тестировании модели
+with torch.no_grad():
+    # Оцениваем качество модели
+    train_data_loader = DataLoader(train_data, batch_size=1024)
+    train_features, train_targets = next(iter(train_data_loader))
+
+    train_features = train_features.to(device)
+    train_model_predictions = torch.argmax(model(train_features), dim=1)
+    print('Точность (accuracy) на обучающей выборке:', accuracy_score(train_targets, train_model_predictions))
+
+    test_data_loader = DataLoader(test_data, batch_size=1024)
+    test_features, test_targets = next(iter(test_data_loader))
+
+    test_features = test_features.to(device)
+    test_model_predictions = torch.argmax(model(test_features), dim=1)
+    print('Точность (accuracy) на тестовой выборке:', accuracy_score(test_targets, test_model_predictions))
